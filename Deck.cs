d28a4b26591ba6244fa164c8193bd7e397cc1797
Ethan <ethanmurray180@gmail.com>
2025-02-06 16:56:33 +0000

    new deck class implemented

---
+++ Deck.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Globalization;
+
+namespace CardEngine
+{
+    class Deck 
+    {
+        private Card[] deck;
+        private int deckSize = 0; //The current size of the deck (will point to the last element in a deck)
+        private bool canGenerate;
+
+        public Deck(int deckSize, bool generate) {
+            this.deck = new Card[deckSize];
+            this.canGenerate = (deckSize == 52) && generate;
+
+            if (generate) {
+                generateDeck();
+                shuffleDeck();
+            }
+        }
+
+        public void addCardToDeck(Card card) {
+            int n = deck.Length;
+            if (deckSize == n) {return;}
+
+            deckSize++; 
+            deck[deckSize] = card;
+        }
+
+        public void popCardFromDeck() {
+            //I guess we can just implicitly 'pop' from the top of the deck by moving down the deck
+            //I'm not expecting this to work well lol
+            deckSize--;
+        }
+
+        public void generateDeck() {
+            if (!canGenerate) {return;}
+
+            foreach (Rank rank in Enum.GetValues(typeof(Rank))) {
+                foreach (Suit suit in Enum.GetValues(typeof(Suit))) {
+                   addCardToDeck(new Card(rank, suit));
+                }
+            }
+        }
+
+        //Fisher-Yates shuffle B)
+        public void shuffleDeck() {
+            Random rand = new Random();
+
+            for (int i = deckSize; i > 0; i--) {
+                int j = rand.Next(0, i);
+                Card tmp = deck[i];
+                deck[i] = deck[j];
+                deck[j] = tmp;
+            }
+        }
+    }
+}
